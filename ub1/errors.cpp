6636891c7b73c0c5e1f874f0433f021627790d15
vd-dragan21 <43203539+vd-dragan21@users.noreply.github.com>
2022-11-03 23:23:12 +0100

    added ub1 files

---
+++ ub1/errors.cpp
@@ -0,0 +1,8 @@
+//ub1, Aufgabe 1a, Paul Bruckert, Valeriia Dragan
+#include <iostream>
+
+int main(int argc, char** argv){
+    std::cout << "Typing is difficult" << std::endl;
+    int ret = 0;
+    return ret;
+}
---
+++ ub1/midnight.cpp
@@ -0,0 +1,27 @@
+//ub1, Aufgabe 2, Paul Bruckert, Valeriia Dragan
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char** argv){
+    double a, b, c;
+    cout << "a = " << flush;
+    cin >> a;
+    cout << "b = " << flush;
+    cin >> b;
+    cout << "c = " << flush;
+    cin >> c;
+    double d = pow(b,2) - 4*a*c;
+    if (d < 0){
+        cout << "Keine Nullstelle gefunden" << endl;
+    } else if (d == 0) {
+        double x = (-b + sqrt(d)/(2*a));
+        cout << "Nur eine Nullstelle gefunden:" << x << endl;
+    } else {
+        double x1 = (-b + sqrt(pow(b,2) - 4*a*c)/(2*a));
+        double x2 = (-b - sqrt(pow(b,2) - 4*a*c)/(2*a));
+        cout << x1 << x2 << endl;
+    }
+    return 0;
+}
